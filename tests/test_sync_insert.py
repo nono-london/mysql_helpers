98cd2cc72d18d0dd526bfa5b529d12945c0d7998
hplaptop <7720585+nono-london@users.noreply.github.com>
2024-04-16 11:32:32 +0200

    using dict

--- tests/test_sync_insert.py
+++ tests/test_sync_insert.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 from mysql_helpers.mysql_con.mysql_sync import MySQLConnectorNative
 
 TEST_TABLE_NAME: str = "pytest_temp_1"
-
+load_dotenv()
 
 def test_insert_in_temp_table():
-    load_dotenv()
+
 
     # create temp table
     table_upper = MySQLConnectorNative()
@@ -26,9 +26,9 @@ def test_insert_in_temp_table():
                 `proxy_town` varchar(245) DEFAULT NULL,
                 `proxy_speed` int DEFAULT NULL,
                 `error_count` smallint NOT NULL DEFAULT '0',
-                `captcha_detected` enum('True','False') NOT NULL DEFAULT 'False',
+                `error_selenium_count` smallint NOT NULL DEFAULT '0',
                 `proxy_web_name` varchar(45) DEFAULT NULL,
-                `selenium_success` int NOT NULL DEFAULT '0',
+                
             PRIMARY KEY (`proxy_id`),
             UNIQUE KEY `avoid_duplicate` (`proxy_url`,`proxy_port`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
@@ -49,12 +49,12 @@ def test_insert_in_temp_table():
             sql_query=sql_query, sql_variables=sql_variables, close_connection=False
         )
     # check numbe rof records inserted
-    sql_query = f""" SELECT COUNT(*) 
+    sql_query = f""" SELECT COUNT(*) as count
                 FROM {TEST_TABLE_NAME}
                 """
     result = table_upper.fetch_all_as_dicts(sql_query=sql_query, close_connection=True)
-
-    assert result[0][0] == nbr_records
+    print(result)
+    assert result[0]["count"] == nbr_records
 
 
 if __name__ == "__main__":
